577676bc8273e82beff37c384c3e5b05d7cd0c1a
dingxl <dingxl>
2020-01-16 19:59:57 +0800

    allow cors

---
+++ src/main/java/com/starter/filter/CorsFilter.java
@@ -0,0 +1,39 @@
+package com.starter.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, " +
+                "X-Requested-With, Content-Type, Accept, " +
+                "key, secret, access_token, name, password, user_token"
+        );
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
