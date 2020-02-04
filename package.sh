1d56b45c44181e904bbb6c695fba006cb17f86a2
dingxl <dingxl>
2020-02-04 09:10:57 +0800

    build from api_base

--- package.sh
+++ package.sh
@@ -4,7 +4,4 @@
 mvn package -Dmaven.test.skip=true
 
 # copy files
-cd ./deploy
-# cp ../src/main/resources/application.yml ./
-cp ../target/api-0.0.1-SNAPSHOT.jar ./
-cd ..
+cp ./target/api-0.0.1-SNAPSHOT.jar ./deploy
---
+++ pull.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker pull mysql:5
+docker pull adminer:latest
+docker pull redis:4
+docker pull webcenter/activemq:latest
+docker pull elasticsearch:latest
+docker pull registry.cn-shanghai.aliyuncs.com/hellodock/logstash:7.5.2
+docker pull registry.cn-shanghai.aliyuncs.com/jext/starter_api_base:latest
