3bf7c23e7bedef986e5ac7dab2b7509d9b0b6f70
dingxl <dingxl>
2020-02-14 13:57:05 +0800

    指定处理ActiveMQTestMessage

--- src/main/java/com/starter/mq/MqConsumer.java
+++ src/main/java/com/starter/mq/MqConsumer.java
@@ -1,18 +1,29 @@
 package com.starter.mq;
 
 import com.common.util.LogUtil;
+import org.apache.activemq.command.ActiveMQTextMessage;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
 
 @Component
 public class MqConsumer {
     @JmsListener(destination = "starter.queue")
-    public void listenQueue(String msg) {
-        LogUtil.info("Receive queue msg", msg);
+    public void listenQueue(ActiveMQTextMessage msg) {
+        try {
+            LogUtil.info("Receive queue msg", msg.getJMSXMimeType(), msg.getText());
+        } catch (JMSException e) {
+            LogUtil.error(e.getMessage());
+        }
     }
 
     @JmsListener(destination = "starter.topic", containerFactory = "jmsTopicListenerContainerFactory")
-    public void listenTopic(String msg) {
-        LogUtil.info("Receive topic msg", msg);
+    public void listenTopic(ActiveMQTextMessage msg) {
+        try {
+            LogUtil.info("Receive topic msg", msg.getJMSXMimeType(), msg.getText());
+        } catch (JMSException e) {
+            LogUtil.error(e.getMessage());
+        }
     }
 }
